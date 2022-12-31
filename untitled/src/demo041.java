3b23db0a041277a727631f2dde40f87b8f78061e
歪比巴卜 <94788896+1617223820@users.noreply.github.com>
2022-12-31 22:40:34 +0800

    update

--- untitled/src/demo041.java
+++ untitled/src/demo041.java
@@ -1,6 +1,7 @@
 public class demo041 {
     public static void main(String[] args) {
         System.out.println("Hello demo04!");
+        System.out.println("push!");
         System.out.println("pull!");
     }
 }
